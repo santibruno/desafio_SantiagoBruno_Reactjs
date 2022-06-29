2d637c07a839c209ac5b3cac6130547e557fe698
santibruno <santib1502@gmail.com>
2022-06-29 19:51:06 -0300

    desafio2

---
+++ src/components/navBar.js
@@ -0,0 +1,40 @@
+const NavBar =()=>{
+return(
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Bienvenido</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nuevos Ingresos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Catalogo</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Nosotros
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Preguntas Frecuentes</a></li>
+            <li><a class="dropdown-item" href="#">Ayuda</a></li>
+            <li><a class="dropdown-item" href="#">Contacto</a></li>
+          </ul>
+        </li>
+                
+      </ul>
+    </div>
+   
+  </div>
+  
+  
+</nav>
+)
+}
+export default NavBar;
