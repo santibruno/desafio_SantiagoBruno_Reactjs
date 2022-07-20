6dc497e5bac095be977e5f0d0e1450265c45447e
Santi Bruno <santib1502@gmail.com>
2022-07-20 16:38:00 -0300

    desafioItemCount

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
 import Carrousel from '../components/Carrousel';
+import ItemCount from '../components/ItemCount';
 
 const Detail = () => {
     let params = useParams();
@@ -8,7 +9,16 @@ const Detail = () => {
     const [err, setErr] = useState("");
 
     console.log(params)
+    function onAdd(c) {
+        document.getElementById("Stock").innerHTML = (`
+        
+        <h2>El producto fue agregado al carrito ${c} veces</h2>
+        
+        `
 
+        )
+
+    }
     useEffect(() => {
         fetch("https://dummyjson.com/products/" + params.id)
             .then((res) => res.json())
@@ -22,17 +32,22 @@ const Detail = () => {
     return (
         <div>
             {data && (
-                <div>
+                <div className=''>
                     <div className='d-flex justify-content-center'>
                         <Carrousel items={data.images} />
                     </div>
                     <div className='d-flex justify-content-center '>
                         <h3 className='m-1 p-2 border border-primary rounded'>{data.title}</h3>
-                        
+
                         <h4 className='m-1 py-2 px-5 h3 border border-dark rounded'>${data.price}</h4>
                     </div>
+                    <div className='d-flex justify-content-center ' >
+                        <div id='Stock'  className='m-1 p-2 border border-dark'>
+                            <ItemCount stock={data.stock} onAdd={onAdd} />
+                        </div>
+                    </div>
                     <div className='d-flex justify-content-center h5'>
-                    <p>{data.description}</p>
+                        <p>{data.description}</p>
                     </div>
                 </div>
             )}
